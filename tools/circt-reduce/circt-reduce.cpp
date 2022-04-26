7a72a0a35f7a36c5acb02788a7d7626ce255508b
Will Dietz <will.dietz@sifive.com>
2022-04-26 09:49:53 -0500

    circt-reduce: cleanup --help output, put options in category
    Hides unrelated options from LLVM such as from CodeGen,
    making it much easier to find relevant options.

--- tools/circt-reduce/circt-reduce.cpp
+++ tools/circt-reduce/circt-reduce.cpp
@@ -37,42 +37,53 @@ using namespace circt;
 // Options
 //===----------------------------------------------------------------------===//
 
-static cl::opt<std::string>
-    inputFilename(cl::Positional, cl::desc("<input file>"), cl::Required);
+static cl::OptionCategory CRCat("circt-reduce Options");
+
+static cl::opt<std::string> inputFilename(cl::Positional,
+                                          cl::desc("<input file>"),
+                                          cl::Required, cl::cat(CRCat));
 
 static cl::opt<std::string>
     outputFilename("o", cl::init("-"),
-                   cl::desc("Output filename for the reduced test case"));
+                   cl::desc("Output filename for the reduced test case"),
+                   cl::cat(CRCat));
 
 static cl::opt<bool>
     keepBest("keep-best", cl::init(true),
-             cl::desc("Keep overwriting the output with better reductions"));
+             cl::desc("Keep overwriting the output with better reductions"),
+             cl::cat(CRCat));
 
-static cl::opt<bool> skipInitial(
-    "skip-initial", cl::init(false),
-    cl::desc("Skip checking the initial input for interestingness"));
+static cl::opt<bool>
+    skipInitial("skip-initial", cl::init(false),
+                cl::desc("Skip checking the initial input for interestingness"),
+                cl::cat(CRCat));
 
 static cl::opt<bool> listReductions("list", cl::init(false),
-                                    cl::desc("List all available reductions"));
+                                    cl::desc("List all available reductions"),
+                                    cl::cat(CRCat));
 
-static cl::list<std::string> includeReductions(
-    "include", cl::ZeroOrMore,
-    cl::desc("Only run a subset of the available reductions"));
+static cl::list<std::string>
+    includeReductions("include", cl::ZeroOrMore,
+                      cl::desc("Only run a subset of the available reductions"),
+                      cl::cat(CRCat));
 
 static cl::list<std::string>
     excludeReductions("exclude", cl::ZeroOrMore,
-                      cl::desc("Do not run some of the available reductions"));
+                      cl::desc("Do not run some of the available reductions"),
+                      cl::cat(CRCat));
 
 static cl::opt<std::string> testerCommand(
     "test", cl::Required,
-    cl::desc("A command or script to check if output is interesting"));
+    cl::desc("A command or script to check if output is interesting"),
+    cl::cat(CRCat));
 
 static cl::list<std::string>
     testerArgs("test-arg", cl::ZeroOrMore,
-               cl::desc("Additional arguments to the test"));
+               cl::desc("Additional arguments to the test"), cl::cat(CRCat));
 
 static cl::opt<bool> verbose("v", cl::init(true),
-                             cl::desc("Print reduction progress to stderr"));
+                             cl::desc("Print reduction progress to stderr"),
+                             cl::cat(CRCat));
 
 //===----------------------------------------------------------------------===//
 // Tool Implementation
@@ -284,6 +295,10 @@ static LogicalResult execute(MLIRContext &context) {
 /// `execute` function to do the actual work.
 int main(int argc, char **argv) {
   llvm::InitLLVM y(argc, argv);
+
+  // Hide default LLVM options, other than for this tool.
+  // MLIR options are added below.
+  cl::HideUnrelatedOptions(CRCat);
 
   // Parse the command line options provided by the user.
   registerMLIRContextCLOptions();
