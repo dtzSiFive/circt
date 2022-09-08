afbed0eebb82c65865721ce041a341f83c7c866a
Will Dietz <will.dietz@sifive.com>
2022-09-07 20:01:24 -0500

    Comment.

--- tools/firld/firld.cpp
+++ tools/firld/firld.cpp
@@ -1,4 +1,4 @@
-//===- firtool.cpp - The firtool utility for working with .fir files ------===//
+//===- firld.cpp - The firrtl linker --------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,8 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file implements 'firtool', which composes together a variety of
-// libraries in a way that is convenient to work with as a user.
+// Prototype FIRRTL linker.
 //
 //===----------------------------------------------------------------------===//
 
