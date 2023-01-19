d45124ed4cecd798227159a909ace4432cf01573
Sam Shahrestani <sam.shahrestani@morsemicro.com>
2023-01-20 10:33:50 +1100

    [LowerAnnotations] Use enum for RefType behaviour in WiringProblem

--- lib/Dialect/FIRRTL/FIRRTLAnnotationHelper.cpp
+++ lib/Dialect/FIRRTL/FIRRTLAnnotationHelper.cpp
@@ -786,7 +786,8 @@ LogicalResult circt::firrtl::applyGCTDataTaps(const AnnoPathValue &target,
             [&](auto v) { return sinkBuilder.create<AsAsyncResetPrimOp>(v); });
     }
 
-    state.wiringProblems.push_back({sendVal, sink, "", false});
+    state.wiringProblems.push_back(
+        {sendVal, sink, "", WiringProblem::RefTypeUsage::Prefer});
   }
 
   return success();
@@ -874,6 +875,7 @@ LogicalResult circt::firrtl::applyGCTMemTaps(const AnnoPathValue &target,
         "cannot generate the MemTap, wiretap Type does not match the memory "
         "type");
   auto sink = wireTarget->ref.getOp()->getResult(0);
-  state.wiringProblems.push_back({sendVal, sink, "memTap", false});
+  state.wiringProblems.push_back(
+      {sendVal, sink, "memTap", WiringProblem::RefTypeUsage::Prefer});
   return success();
 }
