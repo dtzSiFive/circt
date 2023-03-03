4a8aa32ec78236ac059d05e2deacc14bd39c63e2
Will Dietz <will.dietz@sifive.com>
2023-03-03 16:19:44 -0600

    format

--- lib/Dialect/FIRRTL/FIRRTLFolds.cpp
+++ lib/Dialect/FIRRTL/FIRRTLFolds.cpp
@@ -2840,7 +2840,7 @@ void RefResolveOp::getCanonicalizationPatterns(RewritePatternSet &results,
 
 LogicalResult RefAssignOp::canonicalize(RefAssignOp op,
                                         PatternRewriter &rewriter) {
-   bool replacedAny = false;
+  bool replacedAny = false;
   std::function<bool(OpOperand &)> isRADest = [&](auto &oper) {
     auto ra = dyn_cast<RefAssignOp>(oper.getOwner());
     // Replace if user is not an ref.assign, or if not the dest of the assign.
