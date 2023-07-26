68b67b5bd38dbf45204096d1be28ab102c86409d
Will Dietz <will.dietz@sifive.com>
2023-07-26 17:15:40 -0500

    OM: mlir::ModuleOp

--- include/circt/Dialect/OM/Evaluator/Evaluator.h
+++ include/circt/Dialect/OM/Evaluator/Evaluator.h
@@ -42,7 +42,7 @@ struct Evaluator {
   instantiate(StringAttr className, ArrayRef<ObjectValue> actualParams);
 
   /// Get the Module this Evaluator is built from.
-  hw::HWDesignOp getModule();
+  mlir::ModuleOp getModule();
 
 private:
   /// Evaluate a Value in a Class body according to the small expression grammar
