41c2367feffb0984ab2a6f74386025e4bb86b863
Will Dietz <will.dietz@sifive.com>
2023-08-10 12:49:47 -0500

    no design legal

--- lib/Conversion/ArcToLLVM/LowerArcToLLVM.cpp
+++ lib/Conversion/ArcToLLVM/LowerArcToLLVM.cpp
@@ -483,7 +483,7 @@ LogicalResult LowerArcToLLVMPass::lowerArcToLLVM() {
   LLVMConversionTarget target(getContext());
   LLVMTypeConverter converter(&getContext());
   RewritePatternSet patterns(&getContext());
-  target.addLegalOp<hw::HWDesignOp>();
+  // target.addLegalOp<hw::HWDesignOp>();
   target.addIllegalOp<arc::ModelOp>();
   populateSCFToControlFlowConversionPatterns(patterns);
   populateFuncToLLVMConversionPatterns(converter, patterns);
